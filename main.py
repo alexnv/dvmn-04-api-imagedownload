b14769692a1cef867e3c528c30eecc0781aa8633
Непряхин Алексей <neprjahin.a@roistat.com>
2022-11-16 17:53:36 +0300

    publish photo to Telegram channel.

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     if telegram_bot_token:
         bot = telegram.Bot(token=telegram_bot_token)
         bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+        bot.send_document(chat_id=chat_id, document=open('./images/nasa_epic_epic_1b_20221115003634.png', 'rb'))
