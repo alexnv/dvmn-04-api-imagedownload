3f6aba4ba963ec13aa01e7737861b3bb71f9252e
Непряхин Алексей <neprjahin.a@roistat.com>
2022-11-16 17:50:39 +0300

    publish text to Telegram channel.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
+
+import telegram
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
     load_dotenv()
+    chat_id = '@alexnv_dvmn_bot_test'
+    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    if telegram_bot_token:
+        bot = telegram.Bot(token=telegram_bot_token)
+        bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
