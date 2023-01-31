df793b146dae4778d7c4b992c21fa7a14ca78a5c
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-01-31 11:09:01 +0300

    функции отправки изолированы от переменных окружения

--- publish_photos_to_telegram.py
+++ publish_photos_to_telegram.py
@@ -13,19 +13,19 @@ from dotenv import load_dotenv
 from common_functions import get_imagefolder
 
 
-def publish_photo_to_telegram(image, bot):
-    chat_id = os.environ['TELEGRAM_BOT_CHANEL']
+def publish_photo_to_telegram(image, bot, chat_id):
     with open(image, 'rb') as image_file:
         bot.send_document(chat_id=chat_id, document=image_file)
 
 
-def publish_files_to_telegram(root, files, timeout, bot):
+def publish_files_to_telegram(root, files, timeout, bot, chat_id):
     for file in files:
-        publish_photo_to_telegram(join(root, file), bot)
+        logging.info(f"send photo {join(root, file)} to telegram")
+        publish_photo_to_telegram(join(root, file), bot, chat_id)
         time.sleep(timeout)
 
 
-def publish_photos_to_telegram(timeout, bot):
+def publish_photos_to_telegram(timeout, bot, chat_id):
     first_run = True
     error_delay = 1
     while True:
@@ -33,16 +33,15 @@ def publish_photos_to_telegram(timeout, bot):
             for root, dirs, files in list(os.walk(get_imagefolder())):
                 if not first_run:
                     random.shuffle(files)
-                publish_files_to_telegram(root, files, timeout, bot)
+                publish_files_to_telegram(root, files, timeout, bot, chat_id)
                 first_run = False
-        except (telegram.error.NetworkError, requests.Timeout) as error:
+        except (telegram.error.NetworkError, requests.Timeout, requests.ConnectionError) as error:
             time.sleep(error_delay)
             error_delay = 30
             logging.error(error)
 
 
-def init_telegram_bot():
-    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+def init_telegram_bot(telegram_bot_token):
     if telegram_bot_token:
         return telegram.Bot(token=telegram_bot_token)
 
@@ -57,8 +56,12 @@ def init_args():
 if __name__ == "__main__":
     load_dotenv()
     args = init_args()
-    tgbot = init_telegram_bot()
+
+    chat_id = os.environ['TELEGRAM_BOT_CHANEL']
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+
+    tgbot = init_telegram_bot(telegram_bot_token)
     logging.getLogger().setLevel(logging.INFO)
     if tgbot:
         timeout_in_seconds = int(args.timeout) * 60 * 60
-        publish_photos_to_telegram(timeout_in_seconds, tgbot)
+        publish_photos_to_telegram(timeout_in_seconds, tgbot, chat_id)
