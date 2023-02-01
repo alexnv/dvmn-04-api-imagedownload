e74a952bd44159bf6ba87bd030d24d427f370487
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-02-01 19:40:56 +0300

    улучшен код

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 
 import telegram
 from dotenv import load_dotenv
@@ -12,7 +13,8 @@ if __name__ == '__main__':
     if telegram_bot_token:
         bot = telegram.Bot(token=telegram_bot_token)
         bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-        with open('./images/nasa_epic_epic_1b_20221115003634.png', 'rb') as img:
+        image_path = Path.cwd() / 'images' / 'nasa_epic_epic_1b_20221115003634.png'
+        with open(image_path, 'rb') as img:
             bot.send_document(chat_id=chat_id, document=img)
     else:
         logging.error("Не задано значение переменной окружения TELEGRAM_BOT_TOKEN")
