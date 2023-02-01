38f18fe1d63a910b5ccf9db3c0629b2a3d490272
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-02-01 19:36:56 +0300

    улучшен код

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     load_dotenv()
-    chat_id = os.getenv('TELEGRAM_BOT_CHANEL')
-    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.environ['TELEGRAM_BOT_CHANEL']
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     if telegram_bot_token:
         bot = telegram.Bot(token=telegram_bot_token)
         bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
